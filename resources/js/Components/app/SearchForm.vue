52daf5b40e2bd3df5b115458a880ab131b6cd9ce
BlackCat <neko.sword@gmail.com>
2023-11-23 17:07:33 +0700

    update: search form

--- resources/js/Components/app/SearchForm.vue
+++ resources/js/Components/app/SearchForm.vue
@@ -1,22 +1,52 @@
-
 <template>
   <Head title="Find Restaurant" />
 
-  <form @submit.prevent="searchRestaurant" class="w-[600px] h-[80px] flex items-center mx-auto">
+  <form @submit.prevent="searchRestaurant" class="w-[350px] sm:w-[600px] h-[80px] flex items-center mx-auto">
     <TextInput 
       type="text" 
       class="block w-full mr-2" 
       v-model="form.search" 
-      autocomplete 
+      autocomplete
       placeholder="Search for Restaurant" 
     />
 
-    <PrimaryButton class="h-[40px]">Search</PrimaryButton>
+    <PrimaryButton 
+      class="h-[40px] ml-3" 
+      :class="{ 'opacity-25 cursor-progress': form.processing }"
+      :disable="form.processing"
+    >
+      Search
+    </PrimaryButton>
   </form>
 
-  <div class="w-[600px] mx-auto mt-16">
-    Content
+  <div 
+    class="w-[350px] sm:w-[600px] mx-auto mt-8"
+    v-if="restaurantData.length > 0"
+  >
+    <h1 class="text-right mb-1">Restaurant Result : {{ restaurantData.length }}</h1>
+    <div 
+      class="border-2 border-rose-700 rounded-lg py-4 px-8 mb-8"
+      v-for="restaurant in restaurantData"
+    >
+      <li class="gap-y-10 list-none">
+        <h1 class="sm:inline-block text-xl font-bold">
+          <img src="/fire.svg" alt="Fire Icon" class="w-6 h-6 inline-block">
+          Restaurant name :
+        </h1>
+        <a :href="restaurant.source"
+          target="_blank"
+          class="font-semibold text-xl bg-orange-500 text-white ml-1 p-1 rounded-md hover:underline hover:duration-300">"{{ restaurant.title }}"</a>
+        <h2 class="text-xl mt-4 font-bold">
+          <img src="/pin.svg" alt="Map Pin" class="w-6 h-6 inline-block">
+          Address :
+          <span class="font-normal text-lg">
+            {{ restaurant.address }}
+          </span>
+        </h2>
+      </li>
+    </div>
   </div>
+
 </template>
 
 <script setup>
@@ -24,15 +54,20 @@ import { Head } from '@inertiajs/vue3';
 import TextInput from '@/Components/TextInput.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
 import { useForm } from '@inertiajs/vue3'
+import axios from 'axios';
+import { ref } from 'vue';
 
 const form = useForm({
-  search: '',
-})
+  search: 'Bang sue',
+});
+
+let restaurantData = ref([]);
 
 async function searchRestaurant() {
   try {
-    const response = await form.post(route('search.restaurant'));
-    console.log(response);
+    const response = await axios.post(`http://localhost/api/maps?keyword=${form.search}`);
+    restaurantData.value = response.data[0].result;
+    // console.log(restaurantData.value);
   } catch (error) {
     console.error('Error submitting form:', error);
   }
@@ -40,6 +75,4 @@ async function searchRestaurant() {
 
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
